06f287c1d04560f7056546634908992cf84ce446
Comic Solvency <131397277+ComicBurduli@users.noreply.github.com>
2024-06-09 21:02:07 +0400

    homework1

---
+++ davaleba 1 javascript/davaleba1.js
@@ -0,0 +1,60 @@
+console.log( 'hello world' );
+
+console.log(5 + 5 )
+
+
+console.log(5-5)
+
+
+console.log(5 / 5)
+
+console.log (550 * 222)
+
+let sabasage = 14 ;
+console.log(sabasage+200)
+
+console.log(sabasage)
+sabasage = 6;
+console.log(sabasage)
+sabasage = 5 + 3 * 50 / 20 ;
+console.log(sabasage)
+sabasage = sabasage * 2.3
+console.log(sabasage)
+
+
+
+let totalCourseMonths = 7 ;
+let monthsPassed = 2 ;
+let monthLeft = totalCourseMonths - monthsPassed ;
+console.log (monthLeft) ;
+
+
+const x = 5;
+x = 10
+
+
+let x = 5
+x += 3
+console.log(x)
+
+let y =5
+y++
+console.log(y)
+
+console.log(15>10)
+
+console.log(91>=10)
+
+
+const barrier = 100;
+let sabasResult = 75 ;
+const testResults = sabasResult>=barrier
+console.log(testResults)
+
+
+const testBarrier = 80 ;
+let mathResult = 20 ;
+let englishResults = 40 ;
+let csResults = 0 ;
+const testResults1 = mathResult + englishResults + csResults >= testBarrier;
+console.log(testResults1)
