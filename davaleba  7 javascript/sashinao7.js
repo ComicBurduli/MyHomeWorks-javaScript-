3f6ba9396d32525b054ead6ebe667f2b8ed1e83f
Comic Solvency <131397277+ComicBurduli@users.noreply.github.com>
2024-06-09 21:04:25 +0400

    homework7

---
+++ davaleba  7 javascript/sashinao7.js
@@ -0,0 +1,41 @@
+///1
+const Comic = function(string , num2) {
+    for(let i = 0 ; i <num2 ; i++) {
+        console.log("saba")
+    }
+} 
+Comic("burduli" , 50)
+
+
+ const saba  = function(string , num) {
+    for ( let i = 0 ; i< num ; i++)
+    {
+        console.log("text")
+    }
+ }
+
+
+
+ saba("Saba" , 100)
+
+
+ const saba1  =  function(justtext , ricxvi) {
+    for (let  i = 0 ; i < ricxvi ; i++ ){
+        console.log("comic Solvency")
+    }
+ }
+
+
+ saba1("Comic" , 100)
+
+
+
+
+
+///2
+
+
+let a = [101, 45, 75, 105, 99, 107]
+let result = a.includes(105)
+console.log(result)
+
