743eeb87dce7ef1dada92caa46e8f0f8d2f29cfc
Comic Solvency <131397277+ComicBurduli@users.noreply.github.com>
2024-06-09 21:05:39 +0400

    homework10

---
+++ davaleba 10 javascript/davaleba10.js
@@ -0,0 +1,17 @@
+function myfunc() {
+    let clickbutton = document.getElementById("n2-div") 
+    clickbutton.classList.toggle("n2-div")
+    clickbutton.classList.toggle(".moni-div")
+    
+    
+
+}
+
+
+function myFunction() {
+    document.getElementById("myCheck").click()
+}
+
+
+
+
