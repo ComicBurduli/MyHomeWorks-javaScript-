554fb23cf65c51de2957fcfd2cef7c9aadc13a17
Comic Solvency <131397277+ComicBurduli@users.noreply.github.com>
2024-06-09 21:05:02 +0400

    homework9

---
+++ davaleba 9 javascript/davaleba9.js
@@ -0,0 +1,13 @@
+let x = document.querySelectorAll("h1")
+
+for ( let i = 0  ; i<x.length ; i++) {
+    x[i].style.color = "red";   
+    x[i].style.fontSize = "100px"
+}
+
+
+
+let saba = document.querySelectorAll("h1")
+for (let i = 0 ; i<saba.length ; i++) {
+    let comic = saba[i].style.backgroundColor = "yellow"
+}
