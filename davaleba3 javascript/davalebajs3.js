37f6d78f28b97c112ba6076cf2207c8a5e071e29
Comic Solvency <131397277+ComicBurduli@users.noreply.github.com>
2024-06-09 21:03:03 +0400

    homework3

---
+++ davaleba3 javascript/davalebajs3.js
@@ -0,0 +1,99 @@
+let Hello1 = 12.754
+let hellomain=Hello1.toFixed(1)
+console.log(hellomain)
+
+
+let ITStep = '777'
+console.log(typeof ITStep)
+let helloworld = Number(ITStep)
+console.log(typeof helloworld)
+
+
+let ITstep123 = '5454'
+console.log(typeof ITstep123)
+let saba555 = Number(ITstep123)
+console.log(saba555)
+
+
+
+
+console.log(parseInt('3098px'))
+console.log(parseInt('88years'))
+console.log(parseInt('665{{') )
+console.log(parseInt('1834pxppx8346746'))
+console.log(parseFloat('8.23.2'))
+console.log(parseFloat('6,68,8'))
+
+
+let Welcomeinf1 = 10.6565
+let sABA=Welcomeinf1.toPrecision(3)
+console.log(sABA)
+
+let saba12 = 9.999
+let Comic = saba12.toPrecision(3)
+console.log(Comic)
+
+
+
+let x=90
+if(x==100) {
+    console.log("მან  წარმატებით ჩააბარა")
+}
+else if(x<50){
+    console.log("მან ვერ ჩააბარა")
+}
+
+else {
+    console.log("მან  ჩააბარა")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let Drivers = "best F1 Drivers"
+
+switch(Drivers) {
+
+case "Lewis Hamilton" :
+console.log('Lewis Hamilton is the Goat ')
+break
+
+
+case "kimi raikkonen" :
+console.log("kimi raikkonen is  one of the Best Driver")
+break
+
+
+case "Michael Schumacher" :
+    console.log("Michael Schumacher is  one of the Best Driver")
+    break
+
+
+
+case "Sebastian Vettel" :
+    console.log("Sebastian Vettel is  one of the Best Driver")
+    break
+
+
+    case "Ayrton Senna" :
+        console.log("Ayrton Senna is  one of the Best Driver")
+     break
+
+default :
+console.log("Max is a truck driver")
+
+
+
+}
+
